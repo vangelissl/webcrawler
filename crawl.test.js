5cf28b75039ee3178762bee5ff8fb0d857dcd50a
Maria Stashenko <mahryia@gmail.com>
2025-12-07 16:12:51 +0200

    add test

--- crawl.test.js
+++ crawl.test.js
@@ -22,3 +22,9 @@ test('normalizeURL capitals', () => {
 	expect(actual).toEqual(expected);
 });
 
+test('normalizeURL strip http', () => {
+	const input = 'https://BLOG.boot.dev/path';
+	const actual = normalizeURL(input);
+	const expected = 'blog.boot.dev/path';
+	expect(actual).toEqual(expected);
+});
