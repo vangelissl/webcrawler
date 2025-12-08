70dc9d131346508a98268af529c34eac07f40c26
Maria Stashenko <mahryia@gmail.com>
2025-12-08 21:05:54 +0200

    add test for multiple relative and absolute urls

--- crawl.test.js
+++ crawl.test.js
@@ -58,3 +58,20 @@ test('getURLsFromHTML relative', () => {
 	const expected = ['https://blog.boot.dev/path/'];
 	expect(actual).toEqual(expected);
 });
+
+test('getURLsFromHTML both', () => {
+	const inputHTMLBody = `
+	<html>
+		<body>
+			<a href="https://blog.boot.dev/path1/">
+			</a>
+			<a href="/path2/">
+				Boot.dev Blog
+			</a>
+		</body>
+	</html>`;
+	const inputBaseURL = "https://blog.boot.dev";
+	const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+	const expected = ['https://blog.boot.dev/path1/', 'https://blog.boot.dev/path2/'];
+	expect(actual).toEqual(expected);
+});
