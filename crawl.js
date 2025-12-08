3fc63a94536914bf86db6b931adb3c58587efc6a
Maria Stashenko <mahryia@gmail.com>
2025-12-08 21:55:41 +0200

    fix header content type check

--- crawl.js
+++ crawl.js
@@ -12,7 +12,7 @@ async function crawlPage(currentURL) {
 		}
 
 		const contentType = resp.headers.get('content-type');
-		if (contentType !== 'text/html') {
+		if (!contentType.includes('text/html')) {
 			console.log(`non html response, content type: ${contentType} on page: ${currentURL}`);
 			return;
 		}
