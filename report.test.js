cad16d77143b97afd28d68d62811b52f6e6aaec6
Maria Stashenko <mahryia@gmail.com>
2025-12-09 15:02:43 +0200

    add test for 5 pages

--- report.test.js
+++ report.test.js
@@ -1,7 +1,7 @@
 const { sortPages } = require('./report.js');
 const { test, expect } = require('@jest/globals');
 
-test('sortPages', () => {
+test('sortPages 2 pages', () => {
 	const input = {
 		'https://wagslane.dev/path': 1,
 		'https://wagslane.dev': 3,
@@ -11,6 +11,28 @@ test('sortPages', () => {
 	const expected = [
 		['https://wagslane.dev', 3],
 		['https://wagslane.dev/path', 1]
+	];
+
+	expect(actual).toEqual(expected);
+});
+
+
+test('sortPages 5 pages', () => {
+	const input = {
+		'https://wagslane.dev/path': 4,
+		'https://wagslane.dev': 3,
+		'https://wagslane.dev/path1': 5,
+		'https://wagslane.dev/path2': 2,
+		'https://wagslane.dev/path3': 1
+	}
+
+	const actual = sortPages(input);
+	const expected = [
+		['https://wagslane.dev/path1', 5],
+		['https://wagslane.dev/path', 4],
+		['https://wagslane.dev', 3],
+		['https://wagslane.dev/path2', 2],
+		['https://wagslane.dev/path3', 1]
 	];
 
 	expect(actual).toEqual(expected);
